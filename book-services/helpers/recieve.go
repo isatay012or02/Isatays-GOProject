ab2dcc4c0f2ca533a502a06d27bde6e33ae61c0b
Isatai <isaa012or02@gmail.com>
2023-12-06 09:33:45 +0600

    [add] Added amqp

---
+++ book-services/helpers/recieve.go
@@ -0,0 +1,61 @@
+package helpers
+
+import (
+	"encoding/json"
+	"log"
+
+	"book-services/entities/domain"
+	books "book-services/repository/book"
+)
+
+func UserCreated() error {
+	queue, errs := Channel.QueueDeclare("user-created", false, false, false, false, nil)
+	if errs != nil {
+		FailOnError(errs, "cant declare queue")
+	}
+	message, err := Channel.Consume(queue.Name, "", true, false, false, false, nil)
+	if err != nil {
+		FailOnError(err, "cant join this consume")
+	}
+	var forever chan struct{}
+
+	go func() {
+		for d := range message {
+			log.Printf("Receive message %s: ", d.Body)
+			var payload domain.Seller
+			err := json.Unmarshal(d.Body, &payload)
+			if err != nil {
+				log.Printf("Something error unmarshal %s:", err)
+			}
+
+			books.NewBookRepository().Seller(payload)
+
+		}
+	}()
+
+	log.Printf("Waiting for messages")
+	<-forever
+	return nil
+}
+
+func UserDeleted() error {
+	queue, errs := Channel.QueueDeclare("user-deleted", false, false, false, false, nil)
+	if errs != nil {
+		FailOnError(errs, "cant declare queue")
+	}
+	message, err := Channel.Consume(queue.Name, "", true, false, false, false, nil)
+	if err != nil {
+		FailOnError(err, "cant join this consume")
+	}
+	var forever chan struct{}
+
+	go func() {
+		for d := range message {
+			log.Printf("Receive message %s: ", d.Body)
+		}
+	}()
+
+	log.Printf("Waiting for messages")
+	<-forever
+	return nil
+}
